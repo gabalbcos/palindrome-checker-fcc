4bcfe604145bf9f3ddf8d331742130b49f1d22bc
gab <114111654+gabalbcos@users.noreply.github.com>
2024-03-11 23:30:46 -0300

    add button functionality to check word, fix case sensitivity of words

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const inputBox = document.getElementById('input-Text-To-Check');
 const resultText = document.getElementById('result-Text');
-
-let wordToCheck = prompt("Please enter the word to check");
+const checkButton = document.getElementById('check-Button');
 
 function checkPalindrome() {
-  if (wordToCheck === wordToCheck.split("").reverse().join("")) {
+  let wordToCheck = inputBox.value;  
+  if (wordToCheck.toLowerCase() === wordToCheck.split("").reverse().join("").toLowerCase()) {
     resultText.textContent = `"${wordToCheck}" is a palindrome`;
   } else {
     resultText.textContent = `"${wordToCheck}" is not a palindrome`;
   }
 }
 
+checkButton.addEventListener('click', checkPalindrome);
