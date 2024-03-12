0ffd85368669d7b161d89407f4786cfdc3b4d29a
gab <114111654+gabalbcos@users.noreply.github.com>
2024-03-11 23:46:33 -0300

    add input validation to avoid empty or spaces in the text input

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const checkButton = document.getElementById('check-Button');
 
 function checkPalindrome() {
   let wordToCheck = inputBox.value;  
+  wordToCheck = wordToCheck.trim()
+  if (wordToCheck.length === 0 ) {
+    resultText.textContent = 'Please write a word to check.';
+  } else {
   if (wordToCheck.toLowerCase() === wordToCheck.split("").reverse().join("").toLowerCase()) {
     resultText.textContent = `"${wordToCheck}" is a palindrome`;
   } else {
     resultText.textContent = `"${wordToCheck}" is not a palindrome`;
   }
+ }
 }
 
 checkButton.addEventListener('click', checkPalindrome);
